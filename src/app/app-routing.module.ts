7e6b9ecb47dd8f252349c8a9c41e0fd929146d9a
Migueleprz <migueleperezc@gmail.com>
2021-12-09 21:49:08 -0500

    Cofiguracion de rutas y se paracion de modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,61 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router'
-import {DashboardComponent} from "./pages/dashboard/dashboard.component";
-import {LoginComponent} from "./auth/login/login.component";
-import {RegisterComponent} from "./auth/register/register.component";
-import {ProgressComponent} from "./pages/progress/progress.component";
-import {Grafica1Component} from "./pages/grafica1/grafica1.component";
-import {Page404Component} from "./pages/page404/page404.component";
-import {PagesComponent} from "./pages/pages.component";
+import {PagesRoutingModule} from "./pages/pages.routing";
+import {AuthRoutingModule} from "./auth/auth.routing";
 
-const routes: Routes = [
-  {
-    path:'',
-    component: PagesComponent,
-    children:[
-      {
-        path: 'panel',
-        component: DashboardComponent
-      },
-      {
-        path:'progress',
-        component: ProgressComponent
-      },
-      {
-        path:'grafica1',
-        component: Grafica1Component
-      },
-      {
-        path:'',
-        redirectTo: '/panel',
-        pathMatch: 'full'
-      }
-    ]
-  },
+import {Page404Component} from "./page404/page404.component";
+
 
 
 
-  {
-    path:'ingreso',
-    component: LoginComponent
-  }
-  ,
-  {
-    path:'registro',
-    component: RegisterComponent
-  },
-  {
-    path:'**',
-    component: Page404Component
-  }
+const routes: Routes = [
+  {path:'', redirectTo:'/panel', pathMatch:'full'},
+  {path:'**', component: Page404Component}
 ];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports:[
     RouterModule
